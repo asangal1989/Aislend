35cf0c76fdd4e1a0446f3a2ffb9f91ed5be44543
asangal1989 <38527367+asangal1989@users.noreply.github.com>
2018-09-12 12:43:36 +0530

    runner updated with jenkins workspace

--- runner.bat
+++ runner.bat
@@ -1,3 +1,4 @@
-set projectLocation=%cd%
+set projectLocation=C:\Program Files (x86)\Jenkins\workspace\sample1
+cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
